863d7e48e5f0d45c03297e2edb8da44455be4666
granzou <loopinf@gmail.com>
2021-08-20 12:10:59 +0900

    Add component tech indicator

--- gb_dots_stock/gb_pipeline_refactor.py
+++ gb_dots_stock/gb_pipeline_refactor.py
@@ -439,7 +439,7 @@ def get_target(
 
     df_target = df_price.groupby('code').apply(lambda df: make_target(df))
     df_target = df_target.reset_index(drop=True)
-    df_target['date'] = df_target.date.str.replace('-', '')
+    # df_target['date'] = df_target.date.str.replace('-', '')
 
     return df_target
 
@@ -460,6 +460,148 @@ def get_target(
   #   'close_60_sma']
     
 
+@component(
+    base_image="gcr.io/deeplearning-platform-release/sklearn-cpu",
+    packages_to_install=["stockstats"]
+)
+def get_techindi(
+  df_price_dataset: Input[Dataset],
+  df_techini_dataset: Output[Dataset]
+):
+  TECHNICAL_INDICATORS_LIST = ['macd',
+  'boll_ub',
+  'boll_lb',
+  'rsi_30',
+  'dx_30',
+  'close_30_sma',
+  'close_60_sma']
+  from stockstats import StockDataFrame as Sdf
+  from sklearn.preprocessing import MaxAbsScaler
+  import pandas as pd
+  class FeatureEngineer:
+    """Provides methods for preprocessing the stock price data
+
+    Attributes
+    ----------
+        use_technical_indicator : boolean
+            we technical indicator or not
+        tech_indicator_list : list
+            a list of technical indicator names (modified from config.py)
+        use_turbulence : boolean
+            use turbulence index or not
+        user_defined_feature:boolean
+            user user defined features or not
+
+    Methods
+    -------
+    preprocess_data()
+        main method to do the feature engineering
+
+    """
+
+    def __init__(
+      self,
+      use_technical_indicator=True,
+      tech_indicator_list=TECHNICAL_INDICATORS_LIST,
+      user_defined_feature=False,
+  ):
+      self.use_technical_indicator = use_technical_indicator
+      self.tech_indicator_list = tech_indicator_list
+      self.user_defined_feature = user_defined_feature
+
+    def preprocess_data(self, df):
+      """main method to do the feature engineering
+      @:param config: source dataframe
+      @:return: a DataMatrices object
+      """
+      #clean data
+      df = self.clean_data(df)
+      
+      # add technical indicators using stockstats
+      if self.use_technical_indicator == True:
+        df = self.add_technical_indicator(df)
+        print("Successfully added technical indicators")
+
+      # add user defined feature
+      if self.user_defined_feature == True:
+        df = self.add_user_defined_feature(df)
+        print("Successfully added user defined features")
+
+      # fill the missing values at the beginning and the end
+      df = df.fillna(method="bfill").fillna(method="ffill")
+      return df
+    
+    def clean_data(self, data):
+      """
+      clean the raw data
+      deal with missing values
+      reasons: stocks could be delisted, not incorporated at the time step 
+      :param data: (df) pandas dataframe
+      :return: (df) pandas dataframe
+      """
+      df = data.copy()
+      df=df.sort_values(['date','tic'],ignore_index=True)
+      df.index = df.date.factorize()[0]
+      merged_closes = df.pivot_table(index = 'date',columns = 'tic', values = 'close')
+      merged_closes = merged_closes.dropna(axis=1)
+      tics = merged_closes.columns
+      df = df[df.tic.isin(tics)]
+      return df
+
+    def add_technical_indicator(self, data):
+      """
+      calculate technical indicators
+      use stockstats package to add technical inidactors
+      :param data: (df) pandas dataframe
+      :return: (df) pandas dataframe
+      """
+      df = data.copy()
+      df = df.sort_values(by=['tic','date'])
+      stock = Sdf.retype(df.copy())
+      unique_ticker = stock.tic.unique()
+
+      for indicator in self.tech_indicator_list:
+        indicator_df = pd.DataFrame()
+        for i in range(len(unique_ticker)):
+          try:
+            temp_indicator = stock[stock.tic == unique_ticker[i]][indicator]
+            temp_indicator = pd.DataFrame(temp_indicator)
+            temp_indicator['tic'] = unique_ticker[i]
+            temp_indicator['date'] = df[df.tic == unique_ticker[i]]['date'].to_list()
+            indicator_df = indicator_df.append(
+                temp_indicator, ignore_index=True
+            )
+          except Exception as e:
+            print(e)
+        df = df.merge(indicator_df[['tic','date',indicator]],on=['tic','date'],how='left')
+      df = df.sort_values(by=['date','tic'])
+      return df
+
+    def add_user_defined_feature(self, data):
+        """
+        add user defined features
+        :param data: (df) pandas dataframe
+        :return: (df) pandas dataframe
+        """
+        df = data.copy()
+        df["daily_return"] = df.close.pct_change(1)
+        df['bb_u_ratio'] = df.boll_ub / df.close
+        df['bb_l_ratio'] = df.boll_lb / df.close
+        df['max_scale_MACD'] = MaxAbsScaler().fit_transform(df[['macd']])
+        # df['return_lag_1']=df.close.pct_change(2)
+        # df['return_lag_2']=df.close.pct_change(3)
+        # df['return_lag_3']=df.close.pct_change(4)
+        # df['return_lag_4']=df.close.pct_change(5)
+        return df
+  
+  df_price = pd.read_csv(df_price_dataset.path)
+  df_price.columns = df_price.columns.str.lower()
+  df_price.rename(columns={'code':'tic'}, inplace=True)
+  fe = FeatureEngineer(user_defined_feature=True)
+  df_process = fe.preprocess_data(df_price)
+  df_process.rename(columns={'tic':'code'}, inplace=True)
+
+  df_process.to_csv(df_techini_dataset.path)
 
 job_file_name='market-data.json'
 @dsl.pipeline(
@@ -481,6 +623,9 @@ def intro_pipeline():
     dic_univ_dataset=op_get_univ.outputs['univ_dataset'],
     )
   op_get_target = get_target(
+    op_get_adj_price.outputs['adj_price_dataset']
+  )
+  op_get_techindi = get_techindi(
     op_get_adj_price.outputs['adj_price_dataset']
   )
   
