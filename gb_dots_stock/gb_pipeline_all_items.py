c64fb2a742dd96af7961ec735d7ca6223f69bfa3
Seunghan-Kim <stop2006@gmail.com>
2021-08-21 17:21:28 +0900

    Add Second Comp : market-info

--- gb_dots_stock/gb_pipeline_all_items.py
+++ gb_dots_stock/gb_pipeline_all_items.py
@@ -30,7 +30,7 @@ def set_defaults()-> NamedTuple(
   'Outputs',
   [
     ('date_ref',str),
-    ('n_days', str)
+    ('n_days', int)
   ]):
 
   import pandas as pd
@@ -38,7 +38,7 @@ def set_defaults()-> NamedTuple(
 
   today = pd.Timestamp.now('Asia/Seoul').strftime('%Y%m%d')
   # today = '20210809'
-  n_days = '20'
+  n_days = 20
 
   cal_KRX = get_calendar('XKRX')
 
@@ -62,90 +62,76 @@ def set_defaults()-> NamedTuple(
   return (date_ref, n_days)
   
 
-# @component(
-#     base_image="gcr.io/dots-stock/python-img-v5.2",
-# )
-# def get_market_info(``
-#     # top30_univ_dataset: Output[Dataset], 
-#     market_info_dataset: Output[Dataset],
-#     today: str,
-#     n_days: int
-# ) -> str:
-#   import pandas as pd
-#   from pandas.tseries.offsets import CustomBusinessDay
-#   from trading_calendars import get_calendar
-#   import functools
+@component(
+    base_image="gcr.io/dots-stock/python-img-v5.2",
+)
+def get_market_info(
+    market_info_dataset: Output[Dataset],
+    date_ref: str,
+    n_days: int
+):
+  import pandas as pd
+  from trading_calendars import get_calendar
+  cal_KRX = get_calendar('XKRX')
 
-#   import pickle
-#   import logging
-#   import networkx as nx
-#   import os
-#   from sqlalchemy import create_engine
+  from sqlalchemy import create_engine  
 
-#   # today = pd.Timestamp.now('Asia/Seoul').strftime('%Y%m%d')
-#   # today = '20210809'
-#   cal_KRX = get_calendar('XKRX')
-#   custombd_KRX = CustomBusinessDay(holidays=cal_KRX.precomputed_holidays)
+  import logging
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.DEBUG)
+  # console handler
+  ch = logging.StreamHandler()
+  ch.setLevel(logging.DEBUG)
+  # create formatter
+  formatter = logging.Formatter(
+      '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  ch.setFormatter(formatter)
+  # add ch to logger
+  logger.addHandler(ch)
 
-#   logger = logging.getLogger(__name__)
-#   logger.setLevel(logging.DEBUG)
-#   # console handler
-#   ch = logging.StreamHandler()
-#   ch.setLevel(logging.DEBUG)
-#   # create formatter
-#   formatter = logging.Formatter(
-#       '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#   ch.setFormatter(formatter)
-#   # add ch to logger
-#   logger.addHandler(ch)
+  # Preference
+  #-----------------------------------------------------------------------------
+  AWS_DB_ID = 'gb_master'
+  AWS_DB_PWD = 'qwert12345'
+  AWS_DB_ADDRESS = 'kwdb-daily.cf6e7v8fhede.ap-northeast-2.rds.amazonaws.com'
+  AWS_DB_PORT = '3306'
+  DB_DATABASE_NAME_daily_naver = 'daily_naver'
+  db_daily_naver_con = create_engine('mysql+pymysql://{0}:{1}@{2}:{3}/{4}?charset=utf8'
+                                      .format(AWS_DB_ID, AWS_DB_PWD, AWS_DB_ADDRESS, AWS_DB_PORT, DB_DATABASE_NAME_daily_naver),
+                                      encoding='utf8',
+                                      echo=False)
 
-#   # Preference
-#   #-----------------------------------------------------------------------------
-#   AWS_DB_ID = 'gb_master'
-#   AWS_DB_PWD = 'qwert12345'
-#   AWS_DB_ADDRESS = 'kwdb-daily.cf6e7v8fhede.ap-northeast-2.rds.amazonaws.com'
-#   AWS_DB_PORT = '3306'
-#   DB_DATABASE_NAME_daily_naver = 'daily_naver'
-#   PROJECT_ID = 'dots-stock'
-#   db_daily_naver_con = create_engine('mysql+pymysql://{0}:{1}@{2}:{3}/{4}?charset=utf8'
-#                                       .format(AWS_DB_ID, AWS_DB_PWD, AWS_DB_ADDRESS, AWS_DB_PORT, DB_DATABASE_NAME_daily_naver),
-#                                       encoding='utf8',
-#                                       echo=False)
+  def get_market_from_naver_aws(date_ref):
+    '''
+    daily naver 에서 db값 그대로 parsing 내용 받아오기
+    '''
+    with db_daily_naver_con.connect() as conn:
+        table_name = f'{date_ref}_daily_allstock_naver'
+        str_sql = f'select * from {table_name} order by 등락률 DESC'
+        df = pd.read_sql_query(str_sql, conn)  # self.get_db_daily_naver_con())
+        df = df.reset_index().rename(columns={'index':'순위_상승률', 'N':'순위_시가총액'})
+        df['순위_상승률'] = df.순위_상승률 + 1
+    return df
 
-#   # @functools.lru_cache()
-#   def get_market_from_naver_aws(date_ref):
-#       '''
-#       daily naver 에서 db값 그대로 parsing 내용 받아오기
-#       '''
-#       with db_daily_naver_con.connect() as conn:
-#           table_name = f'{date_ref}_daily_allstock_naver'
-#           str_sql = f'select * from {table_name} order by 등락률 DESC'
-#           df = pd.read_sql_query(str_sql, conn)  # self.get_db_daily_naver_con())
-#           df = df.reset_index().rename(columns={'index':'순위_상승률', 'N':'순위_시가총액'})
-#           df['순위_상승률'] = df.순위_상승률 + 1
-#       return df
-
-#   def get_krx_on_dates_n_days_ago(date_ref, n_days=20):
-#       return [date.strftime('%Y%m%d')
-#               for date in pd.bdate_range(
-#           end=date_ref, freq='C', periods=n_days,
-#           holidays=cal_KRX.precomputed_holidays) ]
-#   # 1. Market data
-#   #------------------------------------------------------------------------------
-#   def get_markets_aws(date_ref, n_days):
-#       dates_n_days_ago = get_krx_on_dates_n_days_ago(date_ref, n_days)
-#       df_market = pd.DataFrame()
-#       for date in dates_n_days_ago:
-#           df_ = get_market_from_naver_aws(date)
-#           # logger.debug(f'date : {date} and df_.shape {df_.shape}' )
-#           df_market  = df_market.append(df_)
-#       return df_market
+  def get_krx_on_dates_n_days_ago(date_ref, n_days):
+    return [date.strftime('%Y%m%d')
+            for date in pd.bdate_range(
+        end=date_ref, freq='C', periods=n_days,
+        holidays=cal_KRX.precomputed_holidays) ]
   
-#   df_market = get_markets_aws(date_ref=today, n_days=n_days)
+  def get_markets_aws(date_ref, n_days):
+    dates_n_days_ago = get_krx_on_dates_n_days_ago(date_ref, n_days)
+    df_market = pd.DataFrame()
+    for date in dates_n_days_ago:
+        df_ = get_market_from_naver_aws(date)
+        logger.debug(f'date : {date} and df_.shape {df_.shape}' )
+        df_market  = df_market.append(df_)
+    return df_market
+  
+  df_market = get_markets_aws(date_ref=date_ref, n_days=n_days)
 
-#   df_market.to_csv(market_info_dataset.path)
+  df_market.to_csv(market_info_dataset.path)
 
-#   return today
 
 # @component(
 #    base_image="gcr.io/dots-stock/python-img-v5.2"
@@ -800,7 +786,11 @@ job_file_name='ml-with-all-items.json'
   pipeline_root=PIPELINE_ROOT
 )    
 def create_awesome_pipeline():
-  set_defaults()
+  op_set_defaults = set_defaults()
+  op_get_market_info = get_market_info(
+    date_ref = op_set_defaults.outputs['date_ref'],
+    n_days = op_set_defaults.outputs['n_days']
+  )
 
   
   
