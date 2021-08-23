76b48045f7e2fea454763db2445bd09978265807
Seunghan-Kim <stop2006@gmail.com>
2021-08-23 12:13:10 +0900

    add dropna

--- gb_dots_stock/test_code/merge_dataset.py
+++ gb_dots_stock/test_code/merge_dataset.py
@@ -5,7 +5,7 @@ import os
 
 # %%
 folder = '/Users/seunghankim/Downloads/'
-feat = 'pipeline_root_shkim01_516181956427_ml-with-all-items-20210822201701_get-features_-6999174363073216512_features_dataset'
+feat = 'pipeline_root_shkim01_516181956427_ml-with-all-items-20210823113318_get-features_-363683262096211968_features_dataset'
 target = 'pipeline_root_shkim01_516181956427_ml-with-all-items-20210823002313_get-target_-1738969998304477184_df_target_dataset'
 tech = 'pipeline_root_shkim01_516181956427_ml-with-all-items-20210823003125_get-full-tech-indi_217281089834582016_full_tech_indi_dataset'
 
@@ -30,11 +30,26 @@ df_tech = pd.read_csv(path_tech,
                             ).reset_index(drop=True)
 df_tech['date'] = pd.to_datetime(df_tech.date).dt.strftime('%Y%m%d')
 
+#%%
+
 df_ml_dataset = (df_feats.merge(df_target,
                             left_on=['source', 'date'],
-                            right_on=['code', 'date']))
+                            right_on=['code', 'date'],
+                            how='left'))
 
 df_ml_dataset = (df_ml_dataset.merge(df_tech,
                             left_on=['source', 'date'],
-                            right_on=['code', 'date']))
+                            right_on=['code', 'date'],
+                            how='left'))
+
+
+df_ml_dataset.dropna(inplace=True)
+
+
+# %%
+df1 = pd.DataFrame({'a': ['foo', 'bar'], 'b': [1, 2]})
+df2 = pd.DataFrame({'a': ['foo', 'baz'], 'c': [3, 4]})
+
+df1.merge(df2, how='left', on='a')
+
 # %%
