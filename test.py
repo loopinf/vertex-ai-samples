8b537438cf81c6a08d922ab05a6c5b28b5a6a67d
Seunghan-Kim <stop2006@gmail.com>
2021-09-01 09:03:46 +0900

    fix. add 'mkt_cap_cat' to cat_features

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 #%%
 
-path = 'gs://pipeline-dots-stock/pipeline_root/shkim01/516181956427/ml-with-all-items-20210831132626/get-ml-dataset_-3017684429221920768/ml_dataset'
+path = 'gs://pipeline-dots-stock/pipeline_root/shkim01/516181956427/ml-with-all-items-20210831132626/create-model-and-prediction-01_441080084598620160/prediction_result_01'
 
 
 # %%
