302000cd1f24df37e248c0197a95e023bd808180
Seunghan-Kim <stop2006@gmail.com>
2021-09-01 20:18:22 +0900

    ignore this

--- test.py
+++ test.py
@@ -1,11 +1,36 @@
 #%%
+
+# imports
 import pandas as pd
 
 #%%
 
-path = 'gs://pipeline-dots-stock/pipeline_root/shkim01/516181956427/ml-with-all-items-20210831132626/create-model-and-prediction-01_441080084598620160/prediction_result_01'
+today = '20210831'
+
+#%%
+
+# load prediction results
+pred_result_path = 'gs://pipeline-dots-stock/pipeline_root/shkim01/516181956427/ml-with-all-items-20210901105954/create-model-and-prediction-01_-2817977933245710336/prediction_result_01'
+df_pred_results = pd.read_pickle(pred_result_path)
+
+# %%
+
+# get dates
+
+l_dates = df_pred_results.date.unique().tolist()
+
+last_date = l_dates[-1]
+
+if last_date < today :
+    dates_updating = l_dates[]
+
 
 
 # %%
-df = pd.read_pickle(path)
-# %%
+
+# 1. get prediction result (df)
+# select dates
+# 2. get codes from the df
+# 3. get price from fdr
+# 4. shift
+# 5. merge by code and date
