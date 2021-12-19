4f63a713f59cbdc64ddc1c69ad5dc5b94e92e332
granzou <loopinf@gmail.com>
2021-12-19 22:19:29 +0900

    add sql try

---
+++ gb_dots_stock_pipelines/comps_calc_cos_similarity/calc_best_worst_pred_by_pattern.sql
@@ -0,0 +1,77 @@
+  create table red_lion.pattern_eval_by_ as (
+  WITH
+    ordered_ud_r_d0_2_per_code AS (
+    SELECT
+      source_code,
+      variable AS date_passed,
+      ret_p_d0_2_count,
+      ret_p_d0_2_mean,
+      ud_r_d0_2,
+      kernel_size,
+      ROW_NUMBER() OVER (PARTITION BY source_code ORDER BY ud_r_d0_2 DESC, variable ASC) AS rank
+    FROM (
+      SELECT
+        *
+      FROM
+        `dots-stock.red_lion.pattern_eval_20211217`
+      WHERE
+        ud_r_d0_2 > 0.65))
+  SELECT
+    *,
+    "up" AS type
+  FROM
+    ordered_ud_r_d0_2_per_code
+  WHERE
+    rank IN (1,
+      2)
+  ORDER BY
+    ud_r_d0_2 DESC)
+UNION ALL (
+    -- 내린쪽만 하기
+  WITH
+    ordered_ud_r_d0_2_per_code AS (
+    SELECT
+      source_code,
+      variable AS date_passed,
+      ret_p_d0_2_count,
+      ret_p_d0_2_mean,
+      ud_r_d0_2,
+      kernel_size,
+      ROW_NUMBER() OVER (PARTITION BY source_code ORDER BY ud_r_d0_2 ASC, variable ASC) AS rank
+    FROM (
+      SELECT
+        *
+      FROM
+        `dots-stock.red_lion.pattern_eval_20211217`
+      WHERE
+        ud_r_d0_2 < 0.35))
+  SELECT
+    *,
+    "down" AS type
+  FROM
+    ordered_ud_r_d0_2_per_code
+  WHERE
+    rank IN (1,
+      2)
+  ORDER BY
+    ud_r_d0_2 DESC);
+
+SELECT
+  source_code,
+  type,
+  ret_p_d0_2_count,
+  ARRAY_AGG(date_passed) AS date_passed,
+  ARRAY_AGG(ud_r_d0_2) AS ud_r_d0_2,
+  ARRAY_AGG(rank) AS rank
+FROM (
+  SELECT
+    *
+  FROM
+    `dots-stock.red_lion.temp1` 
+    ORDER BY date_passed)
+GROUP BY
+  source_code,
+  type,
+  ret_p_d0_2_count
+LIMIT
+  100
